cdde28bdc8eb00d8a2acaee4aeac357aa59aff4b
palak-bisht049 <124386249+palak-bisht049@users.noreply.github.com>
2024-10-22 10:16:31 +0530

    Your descriptive commit message

---
+++ server/server.js
@@ -0,0 +1,29 @@
+// FRAMEWORKS CONFIGURATION
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
+
+connectDb();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+// ERROR handling middleware
+app.use(errorHandler);
+
+
+
+
+//ROUTES BELOW
+app.get('/',(req,res)=>{
+    res.send("working");
+});
+
+
+
+// APP CONFIG START
+app.listen(port, () =>{
+    console.log('Server running in port http://localhost:${port}');
+});
